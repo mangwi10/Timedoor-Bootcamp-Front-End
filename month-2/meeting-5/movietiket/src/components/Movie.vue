07a9c57eb35e3d860fe674bac6b27c7ce7569e2c
mangwi10 <mankwiyana@gmail.com>
2022-12-19 15:11:16 +0800

    Latihan 3 - props

---
+++ month-2/meeting-5/movietiket/src/components/Movie.vue
@@ -0,0 +1,35 @@
+<template lang="">
+    <div class="movie-detail">
+      <img :src="movieImage" alt="Movie" />
+      <p>{{movieTitle}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ["movie"],
+  data() {
+    return {
+      movieTitle: this.movie.title,
+      movieImage: require(`../assets/${this.movie.imageName}`)
+    }
+  }
+};
+</script>
+
+<style lang="">
+.movie-detail img {
+  border-radius: 20px;
+  width: 230px;
+  height: 350px;
+}
+
+.movie-detail {
+  margin: 7px;
+}
+
+.movie-detail p {
+  margin-top: 15px;
+  font-weight: 1000;
+}
+</style>
