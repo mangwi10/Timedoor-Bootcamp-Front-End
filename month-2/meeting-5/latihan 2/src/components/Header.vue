43559c740b61b3d64144364a71dc895d3c107c66
mangwi10 <mankwiyana@gmail.com>
2022-12-19 14:48:58 +0800

    Latihan 2

---
+++ month-2/meeting-5/latihan 2/src/components/Header.vue
@@ -0,0 +1,85 @@
+<template lang="">
+  <div>
+          <div class="container">
+        <div class="header-content">
+          <div class="header-title">
+            <h1>TIK.ET</h1>
+          </div>
+          <div class="header-menus">
+            <div class="menu navigasi">
+              <ul type="none">
+                <li>
+                  <a href="#">HOME</a>
+                </li>
+                <li>
+                  <a href="#">NEWS</a>
+                </li>
+                <li>
+                  <a href="#">CAREERS</a>
+                </li>
+              </ul>
+            </div>
+            <div class="menu social">
+              <ul type="none">
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-square-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-youtube"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-twitter"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+name: 'Header'
+}
+</script>
+<style>
+  /* Header */
+.header-content {
+  display: flex;
+  justify-content: space-between;
+}
+
+.header-menus {
+  display: flex;
+}
+
+.menu ul {
+  display: flex;
+}
+
+.menu ul li {
+  margin-left: 15px;
+  margin-right: 15px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+}
+
+.menu ul li a {
+  font-weight: 900;
+}
+
+.menu.navigasi ul li:hover a {
+  color: #00abb3;
+}
+</style>
---
+++ month-2/meeting-5/movietiket/src/components/Footer.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+          <div class="footer">
+        <div class="container">
+          <div class="footer-content">
+            <div class="footer-data">
+              <h1>TIK.ET</h1>
+              <p>
+                Best App For Movie Lovers In Indonesia! Movie Entertainment
+                Platform From Cinema To Online Movie Streaming Selections.
+              </p>
+            </div>
+            <div class="footer-data">
+              <ul type="none">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">CAREERS</a></li>
+                <li><a href="#">CONTACT US</a></li>
+                <li><a href="#">PRIVACY POLICY</a></li>
+                <li><a href="#">TERMS & CONDITIONS</a></li>
+              </ul>
+            </div>
+            <div class="footer-data">
+              <div class="footer-socials-text">
+                <p>TIK.ET SUPPORT</p>
+                <p class="info-email">E-MAIL : HELP@TIK.ET</p>
+              </div>
+              <ul type="none" class="footer-socials">
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-square-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-youtube"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-twitter"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <p>2022 TIK.ET - PT MOVIE TIKET. ALL RIGHTS RESERVED.</p>
+        </div>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+<style>
+/* Footer */
+
+    .footer-copyright {
+      padding-top: 8px;
+      padding-bottom: 5px;
+      background-color: #04009a;
+    }
+
+    .footer-copyright p {
+      padding-bottom: 10px;
+      margin-bottom: 0px;
+      text-align: center;
+    }
+
+    .footer {
+      background-color: #03045e;
+      margin-bottom: 0px;
+    }
+
+    .footer,
+    .footer ul li a {
+      color: white;
+    }
+
+    .footer-content {
+      display: flex;
+      justify-content: space-between;
+      padding-top: 10px;
+      padding-bottom: 10px;
+    }
+
+    .footer-content .footer-data {
+      width: 30%;
+    }
+
+    .footer-data ul li {
+      padding-top: 2px;
+      padding-bottom: 2px;
+    }
+
+    .footer-socials {
+      display: flex;
+      justify-content: flex-start;
+      padding: 20px;
+    }
+
+    .footer-socials li {
+      margin-right: 20px;
+    }
+
+    .footer-socials-text {
+      padding: 20px;
+    }
+
+    .footer-socials-text p {
+      margin-top: 0px;
+      margin-bottom: 0px;
+    }
+
+    .info-email {
+      font-size: 13px;
+    }
+</style>
